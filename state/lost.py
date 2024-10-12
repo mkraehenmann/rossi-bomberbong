6ef9f6c840b93f5ff3bfdd79d477d5bf165243c2
Max Kraehenmann <maxkr@student.ethz.ch>
2024-10-12 11:41:42 +0200

    add file uploader

--- state/lost.py
+++ state/lost.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from PIL import Image
+import matplotlib.pyplot as plt
 
 def change_state(state):
     if 'state' in st.session_state:
@@ -6,6 +8,13 @@ def change_state(state):
 
 def lost():
     st.title('You Lost Something ?')
+
+    file = st.file_uploader("Upload an image of the lost item", type=["png", "jpg", "jpeg", "HEIC"])
+
+    if file is not None:
+        img = Image.open(file)
+        img = img.transpose(Image.ROTATE_270)
+        st.image(img)     
 
     # go back to profile page
     st.button('Profile', on_click=change_state, args=['profile'])
